f2b1656d8196fb3764bbfa3e00c8b3293895b214
sinscary <nbsharma@outlook.com>
2016-10-10 23:40:38 +0530

    App default proptypes for title of app

--- app.jsx
+++ app.jsx
@@ -1,10 +1,48 @@
-function Application(){
+function Application(props){
 	return (
-		<div>
-			<h1>Hello from react</h1>
-			<p>I was renderend from the Application container</p>
+		<div className="scoreboard">
+			<div className="header">
+				<h1>{props.title}</h1>
+			</div>
+
+			<div className="players">
+				<div className="player">
+					<div className="player-name">
+						Nitesh Sharma
+					</div>
+					<div className="player-score">
+						<div className="counter">
+							<button className="counter-action decrement"> - </button>
+							<div className="counter-score"> 31 </div>
+							<button className="counter-action increment"> + </button>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className="players">
+				<div className="player">
+					<div className="player-name">
+						Arun Sharma
+					</div>
+					<div className="player-score">
+						<div className="counter">
+							<button className="counter-action decrement"> - </button>
+							<div className="counter-score"> 30 </div>
+							<button className="counter-action increment"> + </button>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 }
 
-ReactDOM.render(<Application/>, document.getElementById('container'));
+Application.propTypes={
+	title: React.PropTypes.string,
+};
+
+Application.defaultProps={
+	title: "ScoreBoard",
+}
+
+ReactDOM.render(<Application />, document.getElementById('container'));
