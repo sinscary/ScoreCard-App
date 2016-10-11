8e16995cd2c3d06e511e46c8792ca9f9e6ebbad5
sinscary <nbsharma@outlook.com>
2016-10-11 15:11:56 +0530

    Add functionality to stopwatch and final commit

--- app.jsx
+++ app.jsx
@@ -19,13 +19,61 @@ var PLAYERS = [
 var nextId = 4;
 
 var Stopwatch = React.createClass({
+	getInitialState: function() {
+	    return {
+	          running: false,
+	          elapsedTime: 0,
+	          previousTime: 0,
+	    };
+	},
+
+	componentDidMount: function() {
+	      this.interval = setInterval(this.onTick, 100);
+	},
+
+	componentWillUnmount: function() {
+	      clearInterval(this.interval);
+	},
+
+	onTick: function(){
+		console.log('onTick');
+		if(this.state.running){
+			var now = Date.now();
+			this.setState({
+				previousTime: now,
+				elapsedTime: this.state.elapsedTime + (now - this.state.previousTime),
+			})
+		}
+	},
+	onStart: function(){
+		this.setState({
+			running: true,
+			previousTime: Date.now(),
+		})
+	},
+	onStop: function(){
+		this.setState({
+			running: false
+
+		})
+	},
+	onReset: function(){
+		this.setState({
+			elapsedTime: 0,
+			previousTime: Date.now(),
+		})
+	},
 	render: function(){
+		var second = Math.floor(this.state.elapsedTime / 1000);
 		return(
 			<div className="stopwatch">
 				<h2>Stopwatch</h2>
-				<div className="stopwatch-time">0</div>
-				<button>Start</button>
-				<button>Reset</button>
+				<div className="stopwatch-time">{second}</div>
+				{this.state.running ? 
+					<button onClick={this.onStop}>Stop</button> 
+					: 
+					<button onClick={this.onStart}>Start</button>}
+				<button onClick={this.onReset}>Reset</button>
 			</div>
 		)
 	}
